33c5cc542c765ce1ebebafa17b07ba056f1fe8ac
Luke Stokes <luke.stokes@gmail.com>
2019-01-15 19:08:56 -0400

    Fixing missing comma

--- frontend/src/pages/hodlers.vue
+++ frontend/src/pages/hodlers.vue
@@ -79,7 +79,7 @@ export default {
         { name: 'account', label: this.$t('account'), field: 'account', align: 'center', searchable:true},
         { name: 'balance', label: 'EOSDAC', field: 'balance' , align: 'left'},
         { name: 'is_member', label: 'Is Member?', field: 'is_member' , align: 'center'},
-        { name: 'has_voted', label: 'Has Voted?', field: 'has_voted' , align: 'center'}
+        { name: 'has_voted', label: 'Has Voted?', field: 'has_voted' , align: 'center'},
         { name: 'is_exchange', label: 'Is an Exchange?', field: 'is_exchange' , align: 'center'}
       ]
     }
